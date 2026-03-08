2b7dcf4d07f39023e3ad64e8c756ca6127da7520
Peter Esveld <peter.esveld@gmail.com>
2026-03-08 13:33:19 -0400

    fix: return response as Uint8Array for OG image endpoint

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -5,7 +5,7 @@ import path from "path";
 export const GET: APIRoute = async () => {
   // Return the default static OG image
   const buffer = fs.readFileSync(path.resolve("./public/astropaper-og.jpg"));
-  return new Response(buffer, {
+  return new Response(new Uint8Array(buffer), {
     headers: { "Content-Type": "image/jpeg" },
   });
 };
